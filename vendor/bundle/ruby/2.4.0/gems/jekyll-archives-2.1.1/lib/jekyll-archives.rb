f1ea5c127ff18da59fa1df2790ea10cb7b97466e
Tiernan OToole <tiernan@tiernanotoole.ie>
2018-03-15 16:09:15 +0000

    updating vendor files

---
+++ vendor/bundle/ruby/2.4.0/gems/jekyll-archives-2.1.1/lib/jekyll-archives.rb
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:a377778cc3f1a008432f2fb2183f85a6ac12eb327ff2563b8416ff20a470476d
+size 3701
---
+++ vendor/bundle/ruby/2.4.0/gems/jekyll-archives-2.1.1/lib/jekyll-archives/archive.rb
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:2be2eb548b5ff62447a6316d0aec1f3007710f9a3b5518ddaa0dda2e9f24ad0f
+size 3448
